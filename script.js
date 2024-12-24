05ff73c98422b0789dbc78eb742133e350c0ef6c
Abdullahkhan-cmd <aaa2345xyd@gmail.com>
2024-12-24 14:37:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,327 @@
+// let one = 3
+// let two =4 
+// let three= 6 
+// let four= 7 
+// let five= 8
+// let average = (one+two+three+four+five)/5
+// console.log('Average of numbers', average)
+
+
+// let Name = 'Abdullah'
+// console.log('"Hello, My name is',Name)
+// let a = 5 
+// let b = 3
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+
+
+
+
+//   function numbers() {
+//     return checker = 19
+    
+//   }
+
+//  if(checker>0){
+//     console.log("Positive Number")
+//  }        
+
+//  else if(checker<0){
+//     console.log("Negative Number")
+//  }
+
+// else if (checker=0){
+//     console.log('Zero')
+// }
+// console.log(numbers(checker))
+
+
+
+
+ 
+
+//  let d = 21
+// let greater = d > 10 ? 'Greater' : 'Not greater'
+//   prompt(greater)
+
+//  alert('not greater')
+
+
+
+// let age = 19
+// let voter = age > 18 ? 'can vote' : 'cant vote'
+// console.log(voter)
+
+
+
+
+// let color = prompt('what is your favourite color?')
+// if(color=== 'red')
+//     alert('mee too')
+
+// else 
+// prompt('really')
+// alert('not bad')
+
+
+// let ages = 19
+// let driver = ages >= 18 ? 'can drive' : 'cant drive';
+// console.log(driver)
+
+// let z= -1
+// let checker = z>0 ? "positive number" : "negative number";
+// console.log(checker)
+
+
+// let firstName = 'Abdullah'
+// let lastName = 'khan'
+// let age = 19
+// let city = 'karachi'
+// console.table({firstName,lastName,age,city})
+
+
+
+// let cost1 = 67
+// let cost2 = 647
+// let cost3 = 228
+// let cost4 = 830
+// console.log(cost1+cost2+cost3+cost4)
+
+
+// // parameter of rectangle
+// let length = 10
+// let breadth = 5
+// let rectangle = (2*length)+(2*breadth)
+// console.log("parameter of rectangle is",rectangle)
+
+
+// let spr = "This is a spring"
+// console.log(spr.indexOf(" "))
+// console.log(spr.substring(3))
+
+
+
+// let lastspr = 'This is a test. This is only a test'
+// console.log(lastspr.lastIndexOf('is'))
+
+
+
+// let marks = '79'
+
+// if (marks>= 80 && marks<=100){
+//     Grade = 'A+'
+// }
+
+//  else if (marks>= 70 && marks<=79){
+//     Grade = 'A'
+// }
+
+// else if (marks>= 60 && marks<=69){
+//     Grade = 'B'
+// }
+
+// else if (marks>= 50 && marks<=59){
+//     Grade = 'C'
+// }
+
+// else if (marks>= 40 && marks<=49){
+//     Grade = 'D'
+// }
+
+
+
+// // Question no 1
+// let greeting = 'Hello,'
+// let name = 'Abdullah'
+// console.log(greeting+name)
+// let age = 5
+// let ageInMonths = (age*12)
+// console.log(ageInMonths)
+
+
+
+
+
+// // Question no 2
+// let num = 9
+// if(num % 2 === 0 ){ checker = 'Even Number'}
+//   else {  checker ='Odd Number'}
+//          console.log(checker)
+
+
+
+// //Question no 1
+// function addThreeNum(num1, num2, num3){
+//     return num1+num2+num3
+// }
+// console.log(addThreeNum(15,20,20))
+
+
+
+// //Question no 2
+// function addThreeNum(num1, num2, num3){
+// return (num1*num2)/num3}
+// console.log(addThreeNum(15,200,20))
+
+
+// //Question no 3
+// function greeting(name){
+// return ("Hello World")}
+// console.log(greeting())
+
+
+// //Question no 4
+// function squareOfNum(b){
+//     return b*b
+// }
+// console.log(squareOfNum(40))
+
+
+// //Question no 5
+// function areaOfRect(l,b){
+//     return l*b
+// }
+// console.log(areaOfRect(40, 10))
+
+
+
+// // Question no 6
+//  function checkNumber(num) {
+//     if (num > 0) {
+//       return "Positive";
+//     } else if (num < 0) {
+//       return "Negative";
+//     } else  {
+//       return "Zero";
+//     }
+//   }
+//   console.log(checkNumber(-5)); 
+//   console.log(checkNumber(5)); 
+//   console.log(checkNumber(0)); 
+
+
+
+
+
+
+// //Question no 1
+// operation =prompt("Enter a operation")
+// a= parseFloat( prompt("enter a first number"))
+// b= parseFloat(prompt("enter a second number"))
+// function Num(n1,n2){
+//   if(operation == 'sub'){
+//   return a-b
+
+ 
+// }
+// else if(operation == 'div')
+//   return a/b
+
+// else if(operation=='add')
+//   return a+b
+
+// else if(operation=='mult')
+//   return a*b
+// }
+
+// alert(`result is ${Num()}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Question no 2
+checkTemp = prompt('Enter a temp in Celcius')
+function checkT(){
+ 
+    return checkTemp*(9/5) + 32
+  
+
+}
+alert(`Temp in fahrenehit ${checkT()}`)
+
+
+
+
+
+
+
+
+
+// //Question no 3
+// userAge = prompt('Enter Your Age')
+// function checkAge(){
+//   if(userAge < 18 )
+//     return "You are minor"
+
+//   else  if(userAge >= 18 && userAge < 60)
+//     return "You are Adult"
+
+//   else {
+//     return "You are senior"
+//   }
+// }
+// console.log(checkAge())
+
+
+// //Question no 4
+// checkNum = prompt('Enter a number')
+// function check(){
+//   if(checkNum%2==0 )
+//     return "Even Number"
+//   else{
+//     return 'odd number'
+//   }
+
+// }
+// console.log(check())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
